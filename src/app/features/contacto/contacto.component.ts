ef989041001d0580f5a2ffdf14463b072deeb6e2
Revillacks <utti212052@utvco.edu.mx>
2025-04-02 10:26:23 -0600

    Agregando contenido de contacto

--- src/app/features/contacto/contacto.component.ts
+++ src/app/features/contacto/contacto.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-contacto',
-  imports: [],
   templateUrl: './contacto.component.html',
-  styleUrl: './contacto.component.scss'
+  styleUrls: ['./contacto.component.scss'],
+  imports: [CommonModule, ReactiveFormsModule]
 })
 export class ContactoComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefono: ['', Validators.required],
+      comentarios: ['']
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      console.log('Datos del formulario:', this.contactForm.value);
+      // Aquí puedes enviar el formulario (por ejemplo, a un API)
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
 }
