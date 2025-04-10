eb0dea661302a4c8f9977b96df137c9d0cf3f7ce
Revillacks <utti212052@utvco.edu.mx>
2025-04-10 06:24:53 -0600

    Agregando funcion de envio de mail para contacto

--- src/app/features/contacto/contacto.component.ts
+++ src/app/features/contacto/contacto.component.ts
@@ -1,32 +1,51 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   standalone: true,
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.scss'],
-  imports: [CommonModule, ReactiveFormsModule]
+  imports: [CommonModule, ReactiveFormsModule],
 })
 export class ContactoComponent {
-  contactForm: FormGroup;
+  /** se declara sin valor inicial */
+  contactForm!: ReturnType<FormBuilder['group']>;
+  sending = false;
+  success = false;
+  error   = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    /* aquí ya existe this.fb */
     this.contactForm = this.fb.group({
       nombre: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      telefono: ['', Validators.required],
-      comentarios: ['']
+      telefono: [
+        '',
+        [Validators.required, Validators.pattern(/^\d{10}$/)],
+      ],
+      comentarios: ['', Validators.required],
     });
   }
 
   onSubmit(): void {
-    if (this.contactForm.valid) {
-      console.log('Datos del formulario:', this.contactForm.value);
-      // Aquí puedes enviar el formulario (por ejemplo, a un API)
-    } else {
+    if (this.contactForm.invalid) {
       this.contactForm.markAllAsTouched();
+      return;
     }
+
+    this.sending = true;
+    this.http
+      .post('http://localhost:3000/contact', this.contactForm.value)
+      .subscribe({
+        next: () => {
+          this.success = true;
+          this.contactForm.reset();
+        },
+        error: () => (this.error = true),
+      })
+      .add(() => (this.sending = false));
   }
 }
